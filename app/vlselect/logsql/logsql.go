6f1a36750e820b57c8276cc550a16cc2017a2565
Aliaksandr Valialkin <valyala@victoriametrics.com>
2025-11-14 21:01:37 +0100

    app/vlselect/logsql: return 403 Forbidden status code from /select/tenant_ids with non-empty AccountID request header
    This will allow properly handle this error at client side (built-in web UI or Grafana plugin for VictoriaLogs)
    by inspecing response status code.
    
    This is a follow-up for the commit 189240643f9526b02faba85317c8a5b85fdc7242
    
    Updates https://github.com/VictoriaMetrics/VictoriaLogs/issues/821
    Updates https://github.com/VictoriaMetrics/victorialogs-datasource/issues/158

--- app/vlselect/logsql/logsql.go
+++ app/vlselect/logsql/logsql.go
@@ -1135,7 +1135,11 @@ func ProcessTenantIDsRequest(ctx context.Context, w http.ResponseWriter, r *http
 		// Security measure - prevent from requesting tenant_ids for requests with the already specified tenant.
 		// This allows enforcing the needed tenants at vmauth side, so they won't have access to /select/tenant_ids endpoint.
 		// See https://docs.victoriametrics.com/victoriametrics/vmauth/#modifying-http-headers
-		httpserver.Errorf(w, r, "The /select/tenant_ids endpoint cannot be requested with non-empty AccountID=%q header", accountID)
+		err := &httpserver.ErrorWithStatusCode{
+			Err:        fmt.Errorf("the /select/tenant_ids enpoint cannot be requested with non-empty AccountID=%q header", accountID),
+			StatusCode: http.StatusForbidden,
+		}
+		httpserver.Errorf(w, r, "%s", err)
 		return
 	}
 
