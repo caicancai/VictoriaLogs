b9adabc3c05f543051e3abcbfb4a12a8f8a67db7
Aliaksandr Valialkin <valyala@victoriametrics.com>
2026-02-11 13:42:22 +0100

    app/vlselect/logsql: consistently sort /select/logsql/facets response in the descending order of hits
    Prevsiously the returned results were improperly sorted.
    
    While at it, make sure `| facets` pipe output is consistently sorted by field value
    for field values with identical hits.

--- lib/logstorage/pipe_facets.go
+++ lib/logstorage/pipe_facets.go
@@ -68,7 +68,8 @@ func (pf *pipeFacets) splitToRemoteAndLocal(timestamp int64) (pipe, []pipe) {
 		| filter field_values_count:<=%d
 		| delete field_values_count
 		| sort by (hits desc) limit %d partition by (field_name)
-		| sort by (field_name, hits desc)`, pf.maxValuesPerField, pf.limit)
+		| sort by (field_name, hits desc, field_value)
+		| fields field_name, field_value, hits`, pf.maxValuesPerField, pf.limit)
 	psLocal := mustParsePipes(psLocalStr, timestamp)
 
 	return &pRemote, psLocal
